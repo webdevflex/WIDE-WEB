d7508cb7bd8418a9ad51d8b1e793596a5e02c7cc
webdevflex <andreyweb30@gmail.com>
2021-09-28 22:20:40 +0300

    zzz

--- #src/js/js.js
+++ #src/js/js.js
@@ -102,84 +102,101 @@ document.addEventListener("DOMContentLoaded", () => {
     // })
 //END
 //ТАБЫ НАШЕЙ КОМАНДЫ СТРАНИЦА О НАС
-    let $tabs = function(target) {
-        let
-            _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
-            _eventTabsShow,
-            _showTab = function(tabsLinkTarget) {
-                var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
-                tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
-                tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
-                tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
-                // если следующая вкладка равна активной, то завершаем работу
-                if (tabsLinkTarget === tabsLinkActive) {
-                    return;
-                }
-                // удаляем классы у текущих активных элементов
-                if (tabsLinkActive !== null) {
-                    tabsLinkActive.classList.remove('tabs__link_active');
-                }
-                if (tabsPaneShow !== null) {
-                    tabsPaneShow.classList.remove('tabs__pane_show');
-                }
-                // добавляем классы к элементам (в завимости от выбранной вкладки)
-                tabsLinkTarget.classList.add('tabs__link_active');
-                tabsPaneTarget.classList.add('tabs__pane_show');
-                document.dispatchEvent(_eventTabsShow);
-            },
-            _switchTabTo = function(tabsLinkIndex) {
-                let tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
-                if (tabsLinks.length > 0) {
-                    if (tabsLinkIndex > tabsLinks.length) {
-                        tabsLinkIndex = tabsLinks.length;
-                    } else if (tabsLinkIndex < 1) {
-                        tabsLinkIndex = 1;
-                    }
-                    _showTab(tabsLinks[tabsLinkIndex - 1]);
-                }
-            };
+    // let $tabs = function(target) {
+    //     let
+    //         _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
+    //         _eventTabsShow,
+    //         _showTab = function(tabsLinkTarget) {
+    //             var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
+    //             tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
+    //             tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
+    //             tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
+    //             // если следующая вкладка равна активной, то завершаем работу
+    //             if (tabsLinkTarget === tabsLinkActive) {
+    //                 return;
+    //             }
+    //             // удаляем классы у текущих активных элементов
+    //             if (tabsLinkActive !== null) {
+    //                 tabsLinkActive.classList.remove('tabs__link_active');
+    //             }
+    //             if (tabsPaneShow !== null) {
+    //                 tabsPaneShow.classList.remove('tabs__pane_show');
+    //             }
+    //             // добавляем классы к элементам (в завимости от выбранной вкладки)
+    //             tabsLinkTarget.classList.add('tabs__link_active');
+    //             tabsPaneTarget.classList.add('tabs__pane_show');
+    //             document.dispatchEvent(_eventTabsShow);
+    //         },
+    //         _switchTabTo = function(tabsLinkIndex) {
+    //             let tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
+    //             if (tabsLinks.length > 0) {
+    //                 if (tabsLinkIndex > tabsLinks.length) {
+    //                     tabsLinkIndex = tabsLinks.length;
+    //                 } else if (tabsLinkIndex < 1) {
+    //                     tabsLinkIndex = 1;
+    //                 }
+    //                 _showTab(tabsLinks[tabsLinkIndex - 1]);
+    //             }
+    //         };
 
-        _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
+    //     _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
 
-        _elemTabs.addEventListener('click', function(e) {
-            let tabsLinkTarget = e.target;
-            // завершаем выполнение функции, если кликнули не по ссылке
-            if (!tabsLinkTarget.classList.contains('tabs__link')) {
-                return;
-            }
-            // отменяем стандартное действие
-            e.preventDefault();
-            _showTab(tabsLinkTarget);
-        });
+    //     _elemTabs.addEventListener('click', function(e) {
+    //         let tabsLinkTarget = e.target;
+    //         // завершаем выполнение функции, если кликнули не по ссылке
+    //         if (!tabsLinkTarget.classList.contains('tabs__link')) {
+    //             return;
+    //         }
+    //         // отменяем стандартное действие
+    //         e.preventDefault();
+    //         _showTab(tabsLinkTarget);
+    //     });
 
-        return {
-            showTab: function(target) {
-                _showTab(target);
-            },
-            switchTabTo: function(index) {
-                _switchTabTo(index);
-            }
-        }
+    //     return {
+    //         showTab: function(target) {
+    //             _showTab(target);
+    //         },
+    //         switchTabTo: function(index) {
+    //             _switchTabTo(index);
+    //         }
+    //     }
 
-    };
-    $tabs('.tabs');
+    // };
+    // $tabs('.tabs');
 // END
+// //показываем -скрываем отзывы (b2b-cabinet.html)
+    let reviewItemAll = document.querySelectorAll('.reviews-item');
+    let btnReview = document.querySelector('.reviews-btnAll');
+    if(btnReview){
+        btnReview.addEventListener('click',function(){
+            reviewItemAll.forEach(elem =>{
+                if(elem.hasAttribute('data-id')){
+                    elem.classList.toggle('show-reviews')
+                }
+                
+            })
+            btnReview.parentNode.removeChild(btnReview)
+        })
+    }
+// //показываем -скрываем отзывы (about.html)
+
+    let reviewsAll = document.querySelectorAll('.reviews-our-clinic-block');
+    let btnReviews = document.querySelector('.btn-more');
+    if(btnReviews){
+        btnReviews.addEventListener('click',function(){
+            reviewsAll.forEach(elem =>{
+                if(elem.hasAttribute('data-id')){
+                    elem.classList.toggle('show-reviews')
+                }  
+            })
+            btnReviews.parentNode.removeChild(btnReviews)
+        })
+    }
+   
+
+
+   
+   
 
 });
 
-//показываем -скрываем отзывы
-
-let reviewsAll = document.querySelectorAll('.reviews-our-clinic-block');
-let btnReviews = document.querySelector('.btn-more');
-
-btnReviews.addEventListener('click',function(){
-    
-   
-    reviewsAll.forEach(elem =>{
-        if(elem.hasAttribute('data-id')){
-            elem.classList.toggle('show-reviews')
-        }
-        
-    })
-    btnReviews.parentNode.removeChild(btnReviews)
-})
